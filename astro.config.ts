adb4c77ee30bd46e5b0b9e6abf8cc9bb1c2410e7
nbabcock <nathan.r.babcock@gmail.com>
2026-02-28 14:36:00 -0600

    feat: iterate on draft of idb post

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import rehypeExternalLinks from "rehype-external-links"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
 export default defineConfig({
+  devToolbar: { enabled: false },
   site: "https://nathanbabcock.dev",
-  integrations: [react(), sitemap()],
+  integrations: [react(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
